cbe5d6304bb30b6a494dde36561e7c859c41564c
junsang-ee <dkwnstkddk@gmail.com>
2023-08-31 17:02:03 +0900

    Add index.js Modify app.vue Add server config In vite.config.js Create MainDashboard vue file

---
+++ src/views/MainDashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+
+defineProps({
+  message: String,
+})
+</script>
+
+<style scoped>
+
+</style>
